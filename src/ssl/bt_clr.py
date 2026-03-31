01c6682c4d9866550cf0de541171542735c70cc9
nirbhay-design <sharma.59@iitj.ac.in>
2026-03-31 05:32:55 +0530

    barlow twins + clr code

---
+++ src/ssl/bt_clr.py
@@ -0,0 +1,49 @@
+import torch 
+import torchvision
+import torch.nn as nn 
+import torch.nn.functional as F 
+
+def train_btclr(
+        model, bt_proj, clr_proj, train_loader, lossfunction_bt, lossfunction_clr, 
+        optimizer, opt_lr_schedular, 
+        n_epochs, device_id, eval_id, return_logs=False, progress=None): 
+    
+    print(f"### Barlow Twins + SimCLR Training begins")
+
+    device = torch.device(f"cuda:{device_id}")
+    model = model.to(device)
+    bt_proj = bt_proj.to(device)
+    clr_proj = clr_proj.to(device)
+
+    for epochs in range(n_epochs):
+        model.train()
+        cur_loss = 0
+        len_train = len(train_loader)
+        for idx , (data, data_cap, target) in enumerate(train_loader):
+            data = data.to(device)
+            data_cap = data_cap.to(device)
+            
+            feats = model(data)
+            feats_cap = model(data_cap)
+
+
+
+            feats, proj_feat = output["features"], output["proj_features"]
+            feats_cap, proj_feat_cap = output_cap["features"], output_cap["proj_features"]
+
+            loss_con = lossfunction(proj_feat, proj_feat_cap)
+            
+            optimizer.zero_grad()
+            loss_con.backward()
+            optimizer.step()
+
+            cur_loss += loss_con.item() / (len_train)
+            
+            if return_logs:
+                progress(idx+1,len(train_loader), loss_con=loss_con.item(), GPU = device_id)
+        
+        opt_lr_schedular.step()
+              
+        print(f"[GPU{device_id}] epochs: [{epochs+1}/{n_epochs}] train_loss_con: {cur_loss:.3f}")
+
+    return model
