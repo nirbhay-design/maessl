bb2b73e85eeec2982118fc91997371024947e1e3
nirbhay-design <sharma.59@iitj.ac.in>
2026-04-04 01:11:44 +0530

    logs for cifar100

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
-# nohup python train.py --config configs/bt.yaml --dataset cifar100 --save_path bt.clr.r18.c100.pth --gpu 0 --model resnet18 --epochs 800 --lr 0.4 --opt LARS --warmup_epochs 40 --distributed > logs/bt.clr.r18.c100.log &
+nohup python train.py --config configs/bt.yaml --dataset cifar100 --save_path bt.clr.r18.c100.pth --gpu 0 --model resnet18 --epochs 800 --lr 0.4 --opt LARS --warmup_epochs 10 --distributed > logs/bt.clr.r18.c100.log &
 
-# nohup python train.py --config configs/vicreg.yaml --dataset cifar100 --save_path vicreg.clr.r18.c100.pth --gpu 0 --model resnet18 --epochs 800 --opt LARS --warmup_epochs 40 --lr 0.4 --distributed --port 8192 > logs/vicreg.clr.r18.c100 &
+nohup python train.py --config configs/vicreg.yaml --dataset cifar100 --save_path vicreg.clr.r18.c100.pth --gpu 0 --model resnet18 --epochs 800 --lr 0.4 --opt LARS --warmup_epochs 10  --distributed --port 8192 > logs/vicreg.clr.r18.c100 &
 
-CUDA_VISIBLE_DEVICES=0,2 OPENBLAS_NUM_THREADS=1 nohup python train.py --config configs/bt.yaml --dataset timg --save_path bt.clr.r18.timg.pth --gpu 0 --model resnet18 --epochs 800 --lr 0.4 --opt LARS --warmup_epochs 10 --distributed > logs/bt.clr.r18.timg &
+# CUDA_VISIBLE_DEVICES=0,2 OPENBLAS_NUM_THREADS=1 nohup python train.py --config configs/bt.yaml --dataset timg --save_path bt.clr.r18.timg.pth --gpu 0 --model resnet18 --epochs 800 --lr 0.4 --opt LARS --warmup_epochs 10 --distributed > logs/bt.clr.r18.timg &
 
-CUDA_VISIBLE_DEVICES=0,2 OPENBLAS_NUM_THREADS=1 nohup python train.py --config configs/vicreg.yaml --dataset timg --save_path vicreg.clr.r18.timg.pth --gpu 0 --model resnet18 --epochs 800 --lr 0.4 --opt LARS --warmup_epochs 10  --distributed --port 5192 > logs/vicreg.clr.r18.timg &
+# CUDA_VISIBLE_DEVICES=0,2 OPENBLAS_NUM_THREADS=1 nohup python train.py --config configs/vicreg.yaml --dataset timg --save_path vicreg.clr.r18.timg.pth --gpu 0 --model resnet18 --epochs 800 --lr 0.4 --opt LARS --warmup_epochs 10  --distributed --port 5192 > logs/vicreg.clr.r18.timg &
