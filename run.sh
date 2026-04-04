c01bfdf8129aac86633a7e35a0aea42c77b7c4b8
nirbhay-design <sharma.59@iitj.ac.in>
2026-04-05 00:46:04 +0530

    running cifar100 training ddp

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
-nohup python train.py --config configs/bt_clr.yaml --dataset cifar100 --save_path bt.clr.r18.c100.pth --gpu 0 --model resnet18 --epochs 800 --lr 0.2 --opt LARS --wd 1e-6 --warmup_epochs 10 --distributed > logs/bt.clr.r18.c100.log &
+CUDA_VISIBLE_DEVICES=2,3 nohup python train.py --config configs/bt_clr.yaml --dataset cifar100 --save_path bt.clr.r18.c100.pth --gpu 2 --model resnet18 --epochs 800 --lr 0.2 --opt LARS --wd 1e-6 --warmup_epochs 10 --distributed > logs/bt.clr.r18.c100.log &
 
-nohup python train.py --config configs/vicreg_clr.yaml --dataset cifar100 --save_path vicreg.clr.r18.c100.pth --gpu 0 --model resnet18 --epochs 800 --lr 0.2 --wd 1e-6 --opt LARS --warmup_epochs 10  --distributed --port 8192 > logs/vicreg.clr.r18.c100 &
+CUDA_VISIBLE_DEVICES=2,3 nohup python train.py --config configs/vicreg_clr.yaml --dataset cifar100 --save_path vicreg.clr.r18.c100.pth --gpu 3 --model resnet18 --epochs 800 --lr 0.2 --wd 1e-6 --opt LARS --warmup_epochs 10  --distributed --port 8192 > logs/vicreg.clr.r18.c100 &
 
 # CUDA_VISIBLE_DEVICES=0,2 OPENBLAS_NUM_THREADS=1 nohup python train.py --config configs/bt_clr.yaml --dataset timg --save_path bt.clr.r18.timg.pth --gpu 2 --model resnet18 --epochs 800 --lr 0.2 --opt LARS --wd 1e-6 --warmup_epochs 10 --distributed > logs/bt.clr.r18.timg &
 
