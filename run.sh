0fb21668ca5689d7e0457485c9c3aba5b478384c
nirbhay-design <sharma.59@iitj.ac.in>
2026-04-10 00:38:25 +0530

    logs for c100 and timg and mae + bt

--- run.sh
+++ run.sh
@@ -8,4 +8,6 @@
 
 # CUDA_VISIBLE_DEVICES=0,1 OPENBLAS_NUM_THREADS=1 nohup python train.py --config configs/vicreg_clr.yaml --dataset timg --save_path vicreg.clr.r18.timg.pth --gpu 1 --model resnet18 --epochs 800 --lr 0.2 --opt LARS --wd 1e-6 --warmup_epochs 10 > logs/vicreg.clr.r18.timg &
 
-nohup python train.py --config configs/mae_base.yaml --dataset img100 --gpu 0 --epochs 400 --lr 1.5e-4 --opt AdamW --wd 0.05 --warmup_epochs 40 --bs 512 --tbs 512 --save_path mae.i100.pth --model vit > logs/mae.i100.log &
+# nohup python train.py --config configs/mae_base.yaml --dataset img100 --gpu 0 --epochs 400 --lr 1.5e-4 --opt AdamW --wd 0.05 --warmup_epochs 40 --bs 512 --tbs 512 --save_path mae.i100.pth --model vit > logs/mae.i100.log &
+
+nohup python train.py --config configs/mae_bt.yaml --dataset img100 --gpu 0 --epochs 400 --lr 1.5e-4 --opt AdamW --wd 0.05 --warmup_epochs 40 --bs 512 --tbs 512 --save_path mae.bt.i100.pth --model vit > logs/mae.bt.i100.log &
