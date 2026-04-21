f00c52bc7bfe8e8cd96da77495011f122ea2caeb
nirbhay-design <sharma.59@iitj.ac.in>
2026-04-22 00:12:04 +0530

    logs for mae + rotnet

--- run.sh
+++ run.sh
@@ -70,6 +70,10 @@
 
 # OPENBLAS_NUM_THREADS=1 nohup python -u test.py --dataset timg --saved_path saved_models/mae.clr.timg.wt0.5.pth --gpu 1 --model vit --linprobe --lreg --knn --cmet --nw 4 >> logs/mae.clr.timg.wt0.5.log &
 
+OPENBLAS_NUM_THREADS=1 nohup python -u test.py --dataset img100 --saved_path saved_models/mae.mask.rot.i100.wt0.5.pth --gpu 2 --model vit --linprobe --lreg --knn --cmet --nw 6 >> logs/mae.mask.rot.i100.wt0.5.log &
+
+OPENBLAS_NUM_THREADS=1 nohup python -u test.py --dataset timg --saved_path saved_models/mae.mask.rot.timg.wt0.5.pth --gpu 2 --model vit --linprobe --lreg --knn --cmet --nw 6 >> logs/mae.mask.rot.timg.wt0.5.log &
+
 
 ## testing checkpoint models
 
