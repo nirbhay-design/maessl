20d05af6f132163ee452c1fceb2c3e7c8b3360c6
nirbhay-design <sharma.59@iitj.ac.in>
2026-04-22 10:38:09 +0530

    updating run.sh

--- run.sh
+++ run.sh
@@ -43,6 +43,9 @@
 
 # nohup python -u train.py --config configs/mae_bt_rot.yaml --dataset img100 --gpu 1 --epochs 400 --lr 1.5e-4 --opt AdamW --wd 0.05 --warmup_epochs 20 --bs 512 --tbs 512 --save_path mae.bt.rot.i100.pth --model vit --nw 8 --wt 0.1 --wt2 0.5 > logs/mae.bt.rot.i100.log &
 
+
+# ran it for wt 0.8 as well
+
 # OPENBLAS_NUM_THREADS=1 nohup python -u train.py --config configs/mae_clr_rot.yaml --dataset timg --gpu 0 --epochs 400 --lr 1.5e-4 --opt AdamW --wd 0.05 --warmup_epochs 20 --bs 512 --tbs 512 --save_path mae.clr.rot.timg.wt0.1.pth --model vit --nw 8 --wt 0.1 --wt2 0.5 > logs/mae.clr.rot.timg.wt0.1.log &
 
 # OPENBLAS_NUM_THREADS=1 nohup python -u train.py --config configs/mae_clr_rot.yaml --dataset img100 --gpu 1 --epochs 400 --lr 1.5e-4 --opt AdamW --wd 0.05 --warmup_epochs 20 --bs 512 --tbs 512 --save_path mae.clr.rot.i100.wt0.1.pth --model vit --nw 8 --wt 0.1 --wt2 0.5 > logs/mae.clr.rot.i100.wt0.1.log &
