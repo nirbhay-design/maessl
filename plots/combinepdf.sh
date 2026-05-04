793f87a8fb80202f13154e6fc927a3893e65830f
nirbhay-design <sharma.59@iitj.ac.in>
2026-05-04 09:32:31 +0530

    .sh file for combining plots using pdfjam

---
+++ plots/combinepdf.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pdfjam tsne.mae.i100.pdf tsne.mae.bt.i100.pdf tsne.mae.clr.i100.wt0.5.pdf tsne.mae.rot.i100.wt0.5.pdf tsne.mae.clr.rot.i100.wt0.8wt20.1.pdf tsne.mae.bt.rot.i100.pdf --nup 3x2 --scale 0.9 --landscape --outfile tsne_i100.pdf
+
+pdfjam tsne.mae.timg.pdf tsne.mae.bt.timg.pdf tsne.mae.clr.timg.wt0.5.pdf tsne.mae.rot.timg.wt0.5.pdf tsne.mae.clr.rot.timg.wt0.8wt20.1.pdf tsne.mae.bt.rot.timg.pdf --nup 3x2 --scale 0.9 --landscape --outfile tsne_timg.pdf
